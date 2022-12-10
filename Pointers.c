79e2acc62c16d9535ff9e369bedc8edeb0e9a978
Pranav Verma <pranav@verma.net.in>
2022-12-10 23:58:38 +0530

    Update

--- Pointers.c
+++ Pointers.c
@@ -13,10 +13,16 @@
 /*  A pointer is essentially a simple integer variable which holds a memory address that points to a value, 
     instead of holding the actual value itself. The computer's memory is a sequential store of data, and a pointer points to a 
     specific part of the memory. Our program can use pointers in such a way that the pointers point to a large amount of memory 
-    - depending on how much we decide to read from that point on.  */
+    - depending on how much we decide to read from that point on.  
 
     //We've already discussed strings, but now we can dive in a bit deeper and understand what strings in C really are 
     //(which are called C-Strings to differentiate them from other strings when mixed with C++) The following line:
     //char * name = "John";
+
+Dereferencing is the act of referring to where the pointer points, instead of the memory address. We are already using dereferencing 
+in arrays - but we just didn't know it yet. The brackets operator - [0] for example, accesses the first item of the array. 
+And since arrays are actually pointers, accessing the first item in the array is the same as dereferencing a pointer. 
+Dereferencing a pointer is done using the asterisk operator *.
+If we want to create an array that will point to a different variable in our stack, we can write the following code:  *///
 
 
---
+++ Run-Random-Code.c
@@ -0,0 +1,16 @@
+#include <stdio.h> //Always in code
+
+int main() { //Main code
+    int a = 1;
+    int * pointer_to_a = &a;
+
+    /* let's change the variable a */
+    a += 1;
+
+    /* we just changed the variable again! */
+    *pointer_to_a += 1;
+
+    /* will print out 3 */
+    printf("The value of a is now %d\n", *pointer_to_a);
+
+} 
