569ea1d504d17ef529a83788715f2cccf8fdd51b
Pranav Verma <pranav@verma.net.in>
2022-12-10 20:35:07 +0530

    Update If-Else-Statements.c

---
+++ If-Else-Statements.c
@@ -0,0 +1,18 @@
+/*C has the following conditional statements:
+
+    1. Use if to specify a block of code to be executed, if a specified condition is true
+    2. Use else to specify a block of code to be executed, if the same condition is false
+    3. Use else if to specify a new condition to test, if the first condition is false
+    4. Use switch to specify many alternative blocks of code to be executed*/
+#include <stdio.h>
+int main() {
+    
+    int time = 22;
+    if (time < 10) {
+        printf("Good morning.");
+    } else if (time < 20) {
+        printf("Good day.");
+    } else {
+        printf("Good evening.");
+
+}
